3a8891eb7e41dd118230fcd5a8b25f271a7da604
lymilal8 <129526576+lymilal8@users.noreply.github.com>
2023-10-09 00:52:41 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -10,24 +10,27 @@ require('dotenv').config();
 require("./db/mongodb")
 app.use(cors());
 
-const api=require("./routes/movieRoute");
+
 app.use(express.static(path.join("__dirname","./frontend/build")));
-app.get('*',function(_,res){
+
+const api=require("./routes/movieRoute");
+app.use("/api",api);
+const userRouter = require("./routes/userRoute");
+app.use("/api",userRouter);
+const bookingRouter = require("./routes/bookingRoutes");
+app.use("/api",bookingRouter);
+
+app.use('*',function(req,res){
     res.sendFile(path.join(__dirname,"./frontend/build/index.html"),function(err){
         res.status(500).send(err);
     })
 })
-app.use("/api",api);
 
-// const loginRouter = require("./routes/loginRoute");
-// app.use("/api",loginRouter)
-
-const userRouter = require("./routes/userRoute");
-app.use("/api",userRouter);
-
-const bookingRouter = require("./routes/bookingRoutes");
-app.use("/api",bookingRouter);
-
+// app.get('*',function(_,res){
+//     res.sendFile(path.join(__dirname,"./frontend/build/index.html"),function(err){
+//         res.status(500).send(err);
+//     })
+// })
 
 
 const PORT=process.env.PORT;
